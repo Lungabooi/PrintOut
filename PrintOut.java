93c127ace8797ccef8501f5b1c17f0814c9c6edf
Lungabooi <62386061+Lungabooi@users.noreply.github.com>
2020-07-16 21:13:26 +0200

    Add files via upload

---
+++ PrintOut.java
@@ -0,0 +1,17 @@
+Public class Myclass
+{
+  public static void
+  main ( String[]args) {
+   System.out.printIn("Name");
+
+   System.out.printIn("Surname");
+
+   System.out.printIn("Learner Number");
+
+   System.out.printIn("Date of Birth");
+
+   System.out.printIn("Parent contact details");
+
+   System.out.printIn("Physical Address");
+  }
+}
